9790d0edfd9d5f7cdcb4a49ec61e91a3e43bbc30
usama hussain <usamahussain281999@gmail.com>
2020-06-22 23:56:51 +0500

    logout button

--- transaction/transaction.js
+++ transaction/transaction.js
@@ -23,17 +23,14 @@ function incomeResult()
     let getDate=document.getElementById('date-tag').value
     let getSelect=document.getElementById('select-tag').value
     let getDescription=document.getElementById('desc-tag').value
-   console.log(getDate)
-   console.log(getSelect)
-   console.log(getDescription)
-   console.log(getAmount)
+
     if(getAmount=='' || getDate=="" || getSelect=="" || getDescription=="")
     {
-        document.getElementById('modal-error').innerHTML='Fill All Fields'
-        document.getElementById('modal-error').style.backgroundColor='red'
-        document.getElementById('modal-error').style.color='white'
-        document.getElementById('modal-error').style.padding='2%'
-        setTimeout(()=>document.getElementById('modal-error').remove(),2000)
+        document.getElementById('income-error').innerHTML='Fill All Fields'
+        document.getElementById('income-error').style.backgroundColor='red'
+        document.getElementById('income-error').style.color='white'
+        document.getElementById('income-error').style.padding='2%'
+        setTimeout(()=>document.getElementById('income-error').remove(),2000)
         return false
     }
 }
@@ -45,18 +42,19 @@ function expenseResult()
     let getDate=document.getElementById('date2-tag').value
     let getSelect=document.getElementById('select2-tag').value
     let getDescription=document.getElementById('desc2-tag').value
-   console.log(getDate)
-   console.log(getSelect)
-   console.log(getDescription)
-   console.log(getAmount)
+
     if(getAmount=='' || getDate=="" || getSelect=="" || getDescription=="")
     {
-        document.getElementById('modal-error').innerHTML='Fill All Fields'
-        document.getElementById('modal-error').style.backgroundColor='red'
-        document.getElementById('modal-error').style.color='white'
-        document.getElementById('modal-error').style.padding='2%'
-        setTimeout(()=>document.getElementById('modal-error').remove(),2000)
+        document.getElementById('expense-error').innerHTML='Fill All Fields'
+        document.getElementById('expense-error').style.backgroundColor='red'
+        document.getElementById('expense-error').style.color='white'
+        document.getElementById('expense-error').style.padding='2%'
+        setTimeout(()=>document.getElementById('expense-error').remove(),2000)
         return false
     }
-
+    else
+    {
+        alert('43')
+    }
+    
 }
