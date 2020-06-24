b999f5584499d295e255131bb139e8b8b28e271c
usama hussain <usamahussain281999@gmail.com>
2020-06-25 02:35:08 +0500

    tables

--- transaction/transaction.js
+++ transaction/transaction.js
@@ -43,6 +43,7 @@ function incomeResult()
         return incomeToList()
     }
 }
+
 function incomeToList()
 {
     let rightIcon=document.createElement('I')
@@ -55,24 +56,34 @@ function incomeToList()
     upwardIcon.id='income-icon'
     
     let incomeArray=[]
-
     incomeArray.push(document.getElementById('date-tag').value)
     incomeArray.push(document.getElementById('select-tag').value)
-    incomeArray.push( 'RS '+ document.getElementById('amount-tag').value)
+    incomeArray.push( '<span style="font-weight:bold">RS</span> '+ document.getElementById('amount-tag').value)
 
     let bodyElements=document.getElementById('body-data')
     let createRow=document.createElement('TR')
-    createRow.appendChild(rightIcon)
+    let iconTD1=document.createElement('TD')
+    iconTD1.appendChild(rightIcon)
+    createRow.appendChild(iconTD1)
 
     for(var i=0;i<incomeArray.length;i++)
     {
         let createData=document.createElement('TD')
-        createData.innerHTML=incomeArray[i]
-        createRow.appendChild(createData)  
+        createData.innerHTML=incomeArray[i] 
+        createRow.appendChild(createData)
     }
-    createRow.appendChild(upwardIcon)
+    let iconTD2=document.createElement('TD')
+    iconTD2.appendChild(upwardIcon)
+    createRow.appendChild(iconTD2)
     bodyElements.appendChild(createRow)
-
+    clearIncomeFileds()
+}
+function clearIncomeFileds()
+{
+    document.getElementById('date-tag').value=''
+    document.getElementById('select-tag').value=''
+    document.getElementById('amount-tag').value=''
+    document.getElementById('desc-tag').value=''
 }
     
 //function for Expense modal
@@ -93,6 +104,50 @@ function expenseResult()
         setTimeout(()=>document.getElementById('expense-error').remove(),2000)
         return false
     }
-   
+   else
+   {
+       return expenseToList()
+   }    
+}
+function expenseToList()
+{
+    let leftIcon=document.createElement('I')
+    leftIcon.className="fa fa-arrow-left"
     
+    let downIcon=document.createElement('I')
+    downIcon.className="fa fa-arrow-down" 
+
+    leftIcon.id='expense-icon'
+    downIcon.id='expense-icon'
+    
+    let expenseArray=[]
+    expenseArray.push(document.getElementById('date2-tag').value)
+    expenseArray.push(document.getElementById('select2-tag').value)
+    expenseArray.push('<span style="font-weight:bold">RS</span> '+document.getElementById('amount2-tag').value)
+
+    let bodyElements=document.getElementById('body-data')
+    let createExpenseRow=document.createElement('TR')
+    let expenseTD1=document.createElement('TD')
+    expenseTD1.appendChild(leftIcon)
+    createExpenseRow.appendChild(expenseTD1)
+
+    for(var j=0;j<expenseArray.length;j++)
+    {
+        let expenseData=document.createElement('TD')
+        expenseData.innerHTML=expenseArray[j]
+        createExpenseRow.appendChild(expenseData)
+    }
+    let expenseTD2=document.createElement('TD')
+    expenseTD2.appendChild(downIcon)
+    createExpenseRow.appendChild(expenseTD2)
+    bodyElements.appendChild(createExpenseRow)
+    clearExpenseFields()
+}
+function clearExpenseFields()
+{
+    document.getElementById('date2-tag').value=''
+    document.getElementById('select2-tag').value=''
+    document.getElementById('amount2-tag').value=''
+    document.getElementById('desc2-tag').value=''
+
 }
